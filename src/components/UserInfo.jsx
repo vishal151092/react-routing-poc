b8334247e44ca8e7c4b0598e624b225649817327
Vishal Singh <vishal151092@gmail.com>
2023-09-19 22:18:50 +0530

    changes to fetch data

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,18 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const UserInfo = () => {
 
   const {userID} = useParams()
+  const [userData, setUserData] = useState({});
 
   useEffect(()=>{
-    
-  },[])
+    fetch(`https://jsonplaceholder.typicode.com/users/${userID}`)
+    .then((res)=> res.json())
+    .then((data)=>{
+        console.log(data);
+        setUserData(data)
+    })
+  },[userID])
 
 
   return (
     <div style={{border:'1px solid gray',width: '25em', height: '15em'}}>
-       <h3> User Info of <h1 style={{color:'red'}}>User {userID}</h1></h3> 
+       <h3>  <p style={{color:'red'}}>User {userData.name}</p></h3> 
+       <div>
+        <span>{userData.email} | {userData.phone}</span> 
+       </div>
+
+       
     </div>
   )
 }
